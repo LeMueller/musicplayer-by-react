2aeb76a43f96dc2bb6981f878dec99f534a3a0e3
lezhang <lezhang@techfak.uni-bielefeld.de>
2017-09-11 12:23:49 +0200

    header finish, add commen.css and reset.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-//import '../styles/header.less';
+import '../styles/header.less';
+//import logo from '../images/logo.png';
+let logo = require("../images/logo.png");
+
 
 export default class Header extends Component {
 
     render() {
         return (
-            <div className="component-header">
-                <img src={require('../images/logo.png')}  width={40} className="-col-auto"/>
+            <div className="component-header row">
+                <img src={logo}  width={40} className="-col-auto"/>
                 <h1 className="caption">Music Player by React</h1>
             </div>
         );
