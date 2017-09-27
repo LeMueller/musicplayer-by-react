513efb77d52ea5b65931a65ed6f36096efba55f7
lezhang <lezhang@techfak.uni-bielefeld.de>
2017-09-27 17:07:40 +0200

    add testin.. in all css

--- src/components/root.js
+++ src/components/root.js
@@ -8,8 +8,8 @@ import Pubsub from 'pubsub-js';
 
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-require('../styles/css/commen.css');
-require('../styles/css/reset.css');
+import '../styles/css/commen.css';
+import '../styles/css/reset.css';
  
 export default class Root extends Component{
 	constructor(props){
